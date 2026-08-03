ae5ffb8a53abc98672d3c3ed32fedde163eff979
LauraNeira <lauraneira62@gmail.com>
2026-08-03 12:02:28 -0500

    configuración inicial del proyecto de node.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+import express from "express";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
